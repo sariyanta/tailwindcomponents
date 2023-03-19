f82c399747f288cd0dde95a5d62f97896ab5ee7e
Sariyanta <desar@sariyanta.com>
2023-03-19 22:04:12 +0100

    Initial release

---
+++ src/components/Badge.vue
@@ -0,0 +1,3 @@
+<template>
+   <span class="bg-gray-100 text-black inline-block py-2 px-6 rounded-md">Badge</span>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+  
+
+  defineProps({
+    msg: String,
+  })
+
+</script>
+
+<template>
+  <h1 class="text-2xl font-bold">{{ msg }}</h1>
+</template>
+
