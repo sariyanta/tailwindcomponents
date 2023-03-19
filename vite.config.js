5a2c2391eb3e8b286e4875ca55a299ca0cf000db
Sariyanta <desar@sariyanta.com>
2023-03-19 22:12:07 +0100

    Update base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/sariyanta/tailwindcomponents',
+  base: 'https://sariyanta.github.io/tailwindcomponents/',
   plugins: [vue()],
 })
